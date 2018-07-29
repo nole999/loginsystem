3eb03b76e5c01e8ff960eacfe0c0d029d63620e5
nole999 <dragan1945@gmail.com>
2018-07-29 23:03:50 +0200

    first commit

---
+++ header.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+<tittle></tittle>
+<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+
+<header>
+	<nav>
+		<div class="main-wrapper">
+			<ul>
+				<li><a href="index.php">Home</a></li>	
+			</ul>
+		</div>
+		<div class="nav-login">
+			<form>
+				<input type="text" name="uid" placeholder="username/e-mail">
+				<input type="password" name="pwd" placeholder="password">
+				<button type="submit" name="submit">Login</button>
+			</form>
+			<a href="signup.php">Sign up</a>
+		</div>
+	</nav>
+</header>
